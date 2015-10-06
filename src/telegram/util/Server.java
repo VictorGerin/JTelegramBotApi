fddbc72e9e80bc5f85a4d8ab7d97fa4ecfa636ba
Victor Gerin de Lacerda <gerinlacerda@gmail.com>
2015-10-06 09:37:37 -0300

    Add Session time life

--- src/telegram/util/Server.java
+++ src/telegram/util/Server.java
@@ -6,8 +6,12 @@
 package telegram.util;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.Timer;
+import java.util.TimerTask;
 import java.util.function.BiConsumer;
+import java.util.stream.Collectors;
 import telegram.BotApi;
 import telegram.Param;
 import telegram.types.Message;
@@ -20,9 +24,16 @@ import telegram.types.Update;
 public class Server {
 
     private final BotApi bot;
+    private long sessionTime = 30 * 60 * 1000;
     private Executor exec;
     private BiConsumer<Message, Map<String, Object>> execBiFunction;
-    private final Map<String, Map<String, Object>> sesions = new HashMap();
+    private final Map<String, Session> sesions = new HashMap();
+
+    private class Session {
+
+        public Map<String, Object> session = new HashMap();
+        public long lastUpdate = System.currentTimeMillis();
+    }
 
     public interface Executor {
 
@@ -39,33 +50,57 @@ public class Server {
         this.execBiFunction = execBiFunction;
     }
 
+    public void setSessionTime(long sessionTime) {
+        this.sessionTime = sessionTime;
+    }
+
+    public long getSessionTime() {
+        return sessionTime;
+    }
+
     public void start() {
         int offset = 0;
-        while (true) {
-            for (Update update : bot.getUpdates(Param.get("offset", offset))) {
-                int userId = update.getMessage().getFrom().getId();
-                int chatId = update.getMessage().getChatId();
-                String userId_chatId = Integer.toString(userId) + "_" + Integer.toString(chatId);
-                final Map<String, Object> session = sesions.get(userId_chatId);
-                if (session == null) {
-                    final Map<String, Object> _session = new HashMap();
-                    new Thread(() -> {
-                        if (exec != null) {
-                            exec.exec(update.getMessage(), bot, _session);
-                        } else {
-                            execBiFunction.accept(update.getMessage(), _session);
-                        }
-                    }).start();
-                    sesions.put(userId_chatId, _session);
-                } else {
-                    new Thread(() -> {
-                        if (exec != null) {
-                            exec.exec(update.getMessage(), bot, session);
-                        } else {
-                            execBiFunction.accept(update.getMessage(), session);
-                        }
-                    }).start();
+
+        new Timer().schedule(new TimerTask() {
+            @Override
+            public void run() {
+                if (sessionTime >= 0) {
+                    List<String> collect = (sesions)
+                            .keySet()
+                            .stream()
+                            .filter((s) -> System.currentTimeMillis() - sesions.get(s).lastUpdate > sessionTime)
+                            .collect(Collectors.toList());
+
+                    for (String remove : collect) {
+                        sesions.remove(remove);
+                    }
                 }
+            }
+        }, 0, 100);
+
+        while (true) {
+
+            for (Update update : bot.getUpdates(Param.get("offset", offset))) {
+                new Thread(() -> {
+
+                    int userId = update.getMessage().getFrom().getId();
+                    int chatId = update.getMessage().getChatId();
+                    String userId_chatId = Integer.toString(userId) + "_" + Integer.toString(chatId);
+
+                    Session session = sesions.get(userId_chatId);
+                    if (session == null) {
+                        sesions.put(userId_chatId, (session = new Session()));
+                    }
+
+                    if (exec != null) {
+                        exec.exec(update.getMessage(), bot, session.session);
+                    } else {
+                        execBiFunction.accept(update.getMessage(), session.session);
+                    }
+
+                    session.lastUpdate = System.currentTimeMillis();
+                }).start();
+                
                 offset = update.getUpdate_id() + 1;
             }
         }
